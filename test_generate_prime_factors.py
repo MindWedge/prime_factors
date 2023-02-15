03620736bb22efdd90a4e82d40789bf4c5687087
xane <xane@pop-os.localdomain>
2023-02-15 10:36:08 -0500

    First Test Case

--- test_generate_prime_factors.py
+++ test_generate_prime_factors.py
@@ -3,11 +3,12 @@
 The test module for Prime Factors
 """
 
-#First Test Case
+
 import unittest
 from ..prime import generate_prime_factors
 class TestStringMethods(unittest.TestCase):
 
+    #First Test Case
     def test_step1(self):
         with self.assertRaises(ValueError):
             generate_prime_factors(60.5)
